b62254a90e3a216eb406f029280c64b0a5fba266
Никита Воронин <nickvoroninwork@gmail.com>
2025-08-26 16:38:50 +0800

    removed lygia thing and shader itself

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,7 @@ import { extend } from '@react-three/fiber';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { WaterMaterial } from './components/WaterMaterial.jsx';
 import './index.css';
-extend({ WaterMaterial });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
